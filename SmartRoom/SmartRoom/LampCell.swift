d61286eda8359cd89d92cb99c307ec85b4012a66
Joao <sousajoao07@hotmail.com>
2022-01-12 19:22:11 +0000

    WIP: Added LampCell

---
+++ SmartRoom/SmartRoom/LampCell.swift
@@ -0,0 +1,9 @@
+//
+//  LampCell.swift
+//  SmartRoom
+//
+//  Created by João Sousa on 11/01/22.
+//  Copyright © 2022 João Sousa. All rights reserved.
+//
+
+import Foundation
---
+++ SmartRoom/SmartRoom/Models/TableLampCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableLampCell.swift
+//  SmartRoom
+//
+//  Created by João Sousa on 11/01/22.
+//  Copyright © 2022 João Sousa. All rights reserved.
+//
+
+import Foundation
